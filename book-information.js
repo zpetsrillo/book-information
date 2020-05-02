3946924a5f8f68ff5a8801b127e32a878cde6b18
ZPetsrillo <zpetsrillo@gmail.com>
2020-05-02 18:52:40 -0500

    data from amazon v1

--- book-information.js
+++ book-information.js
@@ -1,12 +1,20 @@
 const xlsx = require("xlsx");
 const puppeteer = require("puppeteer");
 
-const wb = xlsx.readFile("books.xls", { cellDates: true });
-const ws = wb.Sheets["Purchases"];
+// const wb = xlsx.readFile("books.xls", { cellDates: true });
+// const ws = wb.Sheets["Purchases"];
 
-const data = xlsx.utils.sheet_to_json(ws);
+// const data = xlsx.utils.sheet_to_json(ws);
 
-// console.log(data);
+// for (let row of data) {
+//   let isbn = row["Pub #"];
+
+//   let isbns = [];
+//   if (isbn && isbn.length >= 10) {
+//     isbn = isbn.replace(/-/g, ``);
+//     isbns.push(isbn);
+//   }
+// }
 
 const escapeXpathString = (str) => {
   const splitedQuotes = str.replace(/'/g, `', "'", '`);
@@ -25,20 +33,94 @@ const clickByText = async (page, text) => {
 };
 
 const getTextFromXPath = async (page, xpath) => {
-  const [el] = await page.$x(xpath);
-  const txt = await el.getProperty("textContent");
-  const rawTxt = await txt.jsonValue();
-  return rawTxt;
+  try {
+    const [el] = await page.$x(xpath);
+    const txt = await el.getProperty("textContent");
+    const rawTxt = await txt.jsonValue();
+    return rawTxt;
+  } catch (error) {
+    return null;
+  }
+};
+
+const getDataFromProductDetails = (productDetails) => {
+  let pages;
+  try {
+    pages = /:\s(\d+)\spages/g.exec(productDetails)[1];
+  } catch (error) {
+    pages = null;
+  }
+
+  let publisher;
+  try {
+    publisher = /Publisher:\s(.*)\n/.exec(productDetails)[1];
+  } catch (error) {
+    publisher = null;
+  }
+
+  let language;
+  try {
+    language = /Language:\s(.*)\n/.exec(productDetails)[1];
+  } catch (error) {
+    language = null;
+  }
+
+  let isbn10;
+  try {
+    isbn10 = /ISBN-10:\s(.*)\n/.exec(productDetails)[1];
+  } catch (error) {
+    isbn10 = null;
+  }
+
+  let isbn13;
+  try {
+    isbn13 = /ISBN-13:\s(.*)\n/.exec(productDetails)[1];
+  } catch (error) {
+    isbn13 = null;
+  }
+
+  let productDimensions;
+  try {
+    productDimensions = /(\d*\.?\d*\sx\s\d*\.?\d*\sx\s\d*\.?\d*\s\w+)/g.exec(
+      productDetails
+    )[1];
+  } catch (error) {
+    productDimensions = null;
+  }
+
+  let shippingWeight;
+  try {
+    shippingWeight = /Shipping Weight:\s(.*)\(/.exec(productDetails)[1];
+  } catch (error) {
+    shippingWeight = null;
+  }
+
+  if (productDimensions) {
+    productDimensions = productDimensions.trim();
+  }
+  if (shippingWeight) {
+    shippingWeight = shippingWeight.trim();
+  }
+
+  return {
+    pages,
+    publisher,
+    language,
+    isbn10,
+    isbn13,
+    productDimensions,
+    shippingWeight,
+  };
 };
 
 const getBookByIsbn = async (page, isbn) => {
   await page.goto(`https://www.amazon.com/s?k=${isbn}&i=stripbooks`);
 
   try {
-    await clickByText(page, `Hardcover`);
+    await clickByText(page, `Paperback`);
   } catch (error) {
     try {
-      await clickByText(page, `Paperback`);
+      await clickByText(page, `Hardcover`);
     } catch (error) {
       return null;
     }
@@ -52,7 +134,26 @@ const getBookByIsbn = async (page, isbn) => {
     '//*[@id="buyNewSection"]/h5/div/div[2]/div/span[2]'
   );
 
-  return { title, price };
+  let author = await getTextFromXPath(page, '//*[@id="bylineInfo"]/span');
+
+  try {
+    author = author.replace(/(\{.*\})/g, "");
+    author = author.trim();
+    author = /((\w|\s)+.*\(\w+\))/g.exec(author)[1];
+    author = author.replace(/(\n|\t)/g, "");
+    author = author.replace(/\s\s+/g, " ");
+  } catch (error) {
+    author = null;
+  }
+
+  author = author.trim();
+
+  const productDetails = await getTextFromXPath(
+    page,
+    '//*[@id="productDetailsTable"]/tbody/tr/td/div/ul'
+  );
+
+  return { title, price, author, productDetails };
 };
 
 const getDataFromAmazon = async (isbns) => {
@@ -60,8 +161,32 @@ const getDataFromAmazon = async (isbns) => {
   const page = await browser.newPage();
 
   for (let isbn of isbns) {
-    let { title, price } = await getBookByIsbn(page, isbn);
-    console.log(title, price);
+    let { title, price, author, productDetails } = await getBookByIsbn(
+      page,
+      isbn
+    );
+    title = title.trim();
+    let {
+      pages,
+      publisher,
+      language,
+      isbn10,
+      isbn13,
+      productDimensions,
+      shippingWeight,
+    } = getDataFromProductDetails(productDetails);
+    console.log({
+      title,
+      price,
+      author,
+      pages,
+      publisher,
+      language,
+      isbn10,
+      isbn13,
+      productDimensions,
+      shippingWeight,
+    });
   }
 
   await browser.close();
